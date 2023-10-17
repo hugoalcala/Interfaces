26dfd1105437ad9381f005aaf705f76c26b24004
Hugo <hugo.pascual.alcala@gmail.com>
2023-10-17 14:13:29 +0200

    interfaces

---
+++ 16deoctubre/app/src/main/java/com/example/a16deoctubre/CONDICIONESDEUSO.java
@@ -0,0 +1,14 @@
+package com.example.a16deoctubre;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CONDICIONESDEUSO extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_condicionesdeuso);
+    }
+}
